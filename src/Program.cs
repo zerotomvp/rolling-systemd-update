15d7d08957ec54ecfd6df69c3c827ddd2a632e73
Georgios Diamantopoulos <georgiosd@gmail.com>
2022-05-01 09:01:20 +0300

    use env vars instead of console args

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,9 @@
-﻿if (args.Length != 6) 
-{
-    throw new ArgumentOutOfRangeException(
-        nameof(args), 
-        string.Join(", ", args),
-        "Expected 6 arguments");
-}
-
-string serviceName = args[0];
-string hosts = args[1];
-string username = args[2];
-int port = int.Parse(args[3]);
-string key = args[4];
-string source = args[5];
+﻿string serviceName = RequireEnvironmentVariable("INPUT_SERVICENAME");
+string hosts = RequireEnvironmentVariable("INPUT_HOSTS");
+string username = RequireEnvironmentVariable("INPUT_USERNAME");
+int port = int.Parse(RequireEnvironmentVariable("INPUT_PORT"));
+string key = RequireEnvironmentVariable("INPUT_KEY");
+string source = RequireEnvironmentVariable("INPUT_SOURCE");
 
 Console.WriteLine(serviceName);
 Console.WriteLine(hosts);
@@ -19,3 +11,15 @@ Console.WriteLine(username);
 Console.WriteLine(port);
 Console.WriteLine(key);
 Console.WriteLine(source);
+
+string RequireEnvironmentVariable(string key)
+{
+    string? value = Environment.GetEnvironmentVariable(key);
+    
+    if (string.IsNullOrEmpty(value))
+    {
+        throw new ArgumentException($"Environment variable {key} is not set");
+    }
+    
+    return value;
+}
